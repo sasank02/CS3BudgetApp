e0ceb6c91664367f4dee913aae6b9fdee6777710
Neel Satasia <satasianeel021@gmail.com>
2021-04-28 11:12:45 -0500

    Added Comment

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.awt.*;
 public class Main {
 
   public static void main(String[] args){
-      JFrame frame = new JFrame("Budget App");
+      JFrame frame = new JFrame("Budget App"); //dffd
       frame.setSize(400, 500);
       frame.setVisible(true);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
